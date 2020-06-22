92cdcb41ef80dff76c84f86051087832d5466a66
David <dayjaycs49@gmail.com>
2020-06-21 20:31:35 -0400

    Added notification to pop on users when one leaves

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -10,3 +10,7 @@ const fireNotification = (text, color) => {
 export const handleNewUser = ({ nickname }) => {
   fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)");
 };
+
+export const handleDisconnected = ({ nickname }) => {
+  fireNotification(`${nickname} just left!`, "rgb(255, 149, 0");
+};
